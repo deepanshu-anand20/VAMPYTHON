1b0680e4907b63f50bc768de1cf32b86286d3d58
DEEPANSHUANAND788 <zachstar8976@gmail.com>
2024-09-26 10:28:36 +0530

    code update

---
+++ LISTDATA.py
@@ -0,0 +1,27 @@
+friendsName = ["Ivan","Anshu","Vani",]
+print(friendsName[2])
+# to print the data of the list we need to use for loop
+print("The name of the respective people in the  ") 
+for x in friendsName :  
+    print(x)
+friendsAge = [10,20,30]
+print("The ages of respective people in the list are: ")
+for x in friendsAge :
+    print(x)
+ #  adding a name in the list 
+friendsName.append("Naman")
+print(friendsName)
+# to add a name on the first spot 
+friendsName.insert(0,"Deepanshu")
+print(friendsName)
+# list can store the duplicate data.
+# to remove the data from the list
+friendsName.remove("Ivan")
+print(friendsName)
+friendsName.append("Raghav")
+friendsName.remove("Deepanshu")
+# remiove cannot declare using index value.
+friendsName.pop()
+print(friendsName)
+friendsName.pop(1)
+print(friendsName)                                         
